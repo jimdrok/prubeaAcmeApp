99461c2c7318432b7e0fd64330c574d713da2ebe
jimdrok <jimdrok@gmail.com>
2021-11-07 21:27:40 -0400

    Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí» .Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí».Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí».Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí».Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí».Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí».Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí».Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí».Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí».Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolor en sí».Lorem Ipsum es un texto que parece latín, pero en realidad no tiene sentido tampoco en este idioma. Su título deriva del latín dolorem ipsum, que significa «el dolo

--- src/app/auth/screens/AuthScreen.js
+++ src/app/auth/screens/AuthScreen.js
@@ -9,8 +9,6 @@ import { loginToken } from '../../../helpers/endPoint';
 import { Context } from '../../../globalContext/globalContext';
 import { useHistory } from 'react-router';
 
-
-
 export default function AuthScreen() {
   let history = useHistory();
   const [loadingLogin, setLoadingLogin] = useState(false)
